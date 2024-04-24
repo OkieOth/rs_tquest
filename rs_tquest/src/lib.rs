f96bb7bc4fbbaf9b0307fb4a8bf55a1d52182bef
Eiko Thomas <eiko.thomas@swarco.com>
2024-04-24 17:17:33 +0200

    wip

--- rs_tquest/src/lib.rs
+++ rs_tquest/src/lib.rs
@@ -1,35 +1,38 @@
 use std::rc::Rc;
 use std::cell::RefCell;
 
-#[derive(Debug)]
+use builder_m4cro::Builder;
+
+#[derive(Debug, Builder)]
 pub struct StringEntry {
+    pub default_value: Option<String>,
     pub reqexp: Option<String>,
     pub max_length: Option<usize>,
+    pub min_length: Option<usize>,
 }
 
-#[derive(Debug)]
+
+
+#[derive(Debug, Builder)]
 pub struct IntEntry {
-    pub max: Option<u32>,
-    pub min: Option<u32>,
+    pub default_value: Option<i32>,
+    pub max: Option<i32>,
+    pub min: Option<i32>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Builder)]
 pub struct FloatEntry {
+    pub default_value: Option<i32>,
     pub max: Option<f32>,
     pub min: Option<f32>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Builder)]
 pub struct BoolEntry {
-    pub input: Option<bool>,
+    pub default_value: Option<bool>,
 }
 
-#[derive(Debug)]
-pub struct ProceedEntry {
-    pub input: Option<bool>,
-}
-
-#[derive(Debug)]
+#[derive(Debug, Builder)]
 pub struct OptionEntry {
     pub options: Vec<String>,
 }
@@ -41,12 +44,14 @@ pub enum EntryType {
     Float(FloatEntry),
     Bool(BoolEntry),
     Option(OptionEntry),
-    ProceedQuery(ProceedEntry),
+    ProceedQuery,
+    InfoTxt,
 }
 
 #[derive(Debug)]
 pub struct QuestionEntry {
     pub query_text: String,
+    pub help_text: Option<String>,
     pub entry_type: EntryType,
     pub id: String,
     pub prev: Option<Rc<RefCell<QuestionEntry>>>,
@@ -75,30 +80,65 @@ pub struct QuestionaireBuilder {
 }
 
 impl QuestionaireBuilder {
-    pub fn add_boolean_question(&mut self, id: &str, query_text: &str, level: u8) -> &mut Self {
+    pub fn add_boolean_question(&mut self, 
+        level: u8,
+        id: &str, 
+        query_text: &str,
+        help_text: Option<&str>,
+        entry_def: Option<BoolEntry>) -> &mut Self {
         self
     }
     
-    pub fn add_string_question(&mut self, id: &str, query_text: &str, level: u8) -> &mut Self {
+    pub fn add_string_question(&mut self,
+        level: u8,
+        id: &str, 
+        query_text: &str,
+        help_text: Option<&str>,
+        entry_def: Option<StringEntry>) -> &mut Self {
         self
     }
 
-    pub fn add_int_question(&mut self, id: &str, query_text: &str, level: u8) -> &mut Self {
+    pub fn add_int_question(&mut self, 
+        level: u8,
+        id: &str, 
+        query_text: &str,
+        help_text: Option<&str>,
+        entry_def: Option<IntEntry>) -> &mut Self {
         self
     }
 
-    pub fn add_float_question(&mut self, id: &str, query_text: &str, level: u8) -> &mut Self {
+    pub fn add_float_question(&mut self, 
+        level: u8,
+        id: &str, 
+        query_text: &str,
+        help_text: Option<&str>,
+        entry_def: Option<FloatEntry>) -> &mut Self {
         self
     }
 
-    pub fn add_option_question(&mut self, id: &str, query_text: &str, level: u8) -> &mut Self {
+    pub fn add_option_question(&mut self,
+        level: u8, 
+        id: &str, 
+        query_text: &str,
+        help_text: Option<&str>,
+        entry_def: Option<OptionEntry>) -> &mut Self {
         self
     }
 
-    pub fn add_proceed_question(&mut self, id: &str, first_query_text: &str, additional_query_text: &str, level: u8) -> &mut Self {
+    pub fn add_proceed_question(&mut self, 
+        level: u8,
+        id: &str, 
+        first_query_text: &str, 
+        additional_query_text: Option<&str>) -> &mut Self {
         self
     }
 
+    pub fn add_info_text(&mut self, 
+        level: u8,
+        id: &str,
+        text: &str) -> &mut Self {
+        self
+    }
 
     pub fn build(&self) -> Questionaire {
         // TODO
