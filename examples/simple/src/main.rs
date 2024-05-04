314e2f008ab8daeb3e8156f8d2484a35bb2040db
Eiko Thomas <13103714+OkieOth@users.noreply.github.com>
2024-05-05 00:12:04 +0200

    wip: build example questionaire

--- examples/simple/src/main.rs
+++ examples/simple/src/main.rs
@@ -14,30 +14,139 @@ fn question_02(builder: &mut QuestionaireBuilder, level: u8, id: &str) {
                     .build().unwrap()));
 }
 
+fn question_03(builder: &mut QuestionaireBuilder, level: u8, id: &str) {
+    builder.add_proceed_question(
+        level,
+        id,
+        "Do you have brothers or sisters?", 
+        Some("Do you have more brothers and sisters?"));
+}
+
+fn question_04(builder: &mut QuestionaireBuilder, level: u8, id: &str) {
+    builder.add_proceed_question(
+        level,
+        id,
+        "Do you have a brother?", 
+        Some("Do you have another brother?"));
+}
+
+fn question_05(builder: &mut QuestionaireBuilder, level: u8, id: &str) {
+    builder.add_string_question(level, id, 
+        "what's the name of your brother?", None, None);
+}
+
+fn question_06(builder: &mut QuestionaireBuilder, level: u8, id: &str) {
+    builder.add_string_question(level, 
+        id,
+        "What's his date of birth?", 
+        Some("Provide the date of birth in YYYY-MM-DD format"), 
+    Some(StringEntry::builder()
+                    .reqexp("\\d\\d\\d\\d-\\d\\d-\\d\\d".to_string())
+                    .build().unwrap()));
+}
+
+fn question_07(builder: &mut QuestionaireBuilder, level: u8, id: &str) {
+    builder.add_proceed_question(
+        level,
+        id,
+        "Do you have a sister?", 
+        Some("Do you have another sister?"));
+}
+
+fn question_08(builder: &mut QuestionaireBuilder, level: u8, id: &str) {
+    builder.add_string_question(level, id, 
+        "what's the name of your sister?", None, None);
+}
+
+fn question_09(builder: &mut QuestionaireBuilder, level: u8, id: &str) {
+    builder.add_string_question(level, 
+        id,
+        "What's her date of birth?", 
+        Some("Provide the date of birth in YYYY-MM-DD format"), 
+    Some(StringEntry::builder()
+                    .reqexp("\\d\\d\\d\\d-\\d\\d-\\d\\d".to_string())
+                    .build().unwrap()));
+}
+
+fn question_10(builder: &mut QuestionaireBuilder, level: u8, id: &str) {
+    builder.add_proceed_question(
+        level,
+        id,
+        "Have you already worked in a job?", 
+        Some("Have you worked in another job?"));
+}
+
+fn question_11(builder: &mut QuestionaireBuilder, level: u8, id: &str) {
+    builder.add_string_question(level, 
+        id,
+        "What was the name of the company you worked for?", 
+        None, 
+    Some(StringEntry::builder()
+                    .min_length(2)
+                    .max_length(200)
+                    .build().unwrap()));
+}
+
+fn question_12(builder: &mut QuestionaireBuilder, level: u8, id: &str) {
+    builder.add_string_question(level, 
+        id,
+        "What was your job title?", 
+        None, 
+    Some(StringEntry::builder()
+                    .min_length(2)
+                    .max_length(200)
+                    .build().unwrap()));
+}
+
+fn question_13(builder: &mut QuestionaireBuilder, level: u8, id: &str) {
+    builder.add_string_question(level, 
+        id,
+        "What was your start date there?", 
+        Some("Provide the year and optional month in YYYY-MM format"), 
+    Some(StringEntry::builder()
+                    .min_length(0)
+                    .reqexp("\\d\\d\\d\\d-\\d\\d".to_string())
+                    .build().unwrap()));
+}
+
+fn question_14(builder: &mut QuestionaireBuilder, level: u8, id: &str) {
+    builder.add_string_question(level, 
+        id,
+        "What was your end date there?", 
+        Some("Provide the year and optional month in YYYY-MM format"), 
+    Some(StringEntry::builder()
+                    .min_length(0)
+                    .reqexp("\\d\\d\\d\\d-\\d\\d".to_string())
+                    .build().unwrap()));
+}
+
+fn question_15(builder: &mut QuestionaireBuilder, level: u8, id: &str) {
+    builder.add_proceed_question(
+        level,
+        id,
+        "Should your answers be stored?", 
+        None);
+}
+
 
 fn build_questionaire() -> Questionaire {
     let mut builder = Questionaire::builder();
     question_01(&mut builder, 0, "id1");
     question_02(&mut builder, 0, "id2");
+    question_03(&mut builder, 0, "id3");
+        question_04(&mut builder, 1, "id4");
+            question_05(&mut builder, 2, "id5");
+            question_06(&mut builder, 2, "id6");
+        question_07(&mut builder, 1, "id7");
+            question_08(&mut builder, 2, "id8");
+            question_09(&mut builder, 2, "id9");
+    question_10(&mut builder, 0, "id10");
+        question_11(&mut builder, 1, "id11");
+        question_12(&mut builder, 1, "id12");
+        question_13(&mut builder, 1, "id13");
+        question_14(&mut builder, 1, "id13");
+    question_14(&mut builder, 0, "id14");
     builder.build()
-
-
-
-    // .add_string_question("2", "What's your date of birth?", None, 0)
-    // .add_proceed_question("3","Do you have brothers or sisters?", "Do you have more brothers and sisters?", 0)
-    //     .add_proceed_question("4", "Do you have a brother?", "Do you have another brother?", 1)
-    //         .add_string_question("5","What's the name of your brother?", 2)
-    //         .add_string_question("6","What's the date of birth of your brother?", 2)
-    //     .add_proceed_question("7","Do you have a sister?", "Do you have another Sister?", 1)
-    //         .add_string_question("8","What's the name of your sister?", 2)
-    //         .add_string_question("9","What's the date of birth of your sister?", 2)
-    // .add_proceed_question("10","Have you worked in a job?", "Have you worked in another job?", 0)
-    //     .add_string_question("11","For what company have you worked?", 1)
-    //     .add_string_question("12","What was your job title?", 1)
-    //     .add_string_question("13","What was your start date?", 1)
-    //     .add_string_question("14","What was your end date?", 1)
-    // .add_proceed_question("15","Should we store the answers?", "", 0)
-    // .build()
 
 }
 
