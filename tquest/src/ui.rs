70c5912e491742ba28c0dca4c9d360cb0d4ad14e
Eiko Thomas <13103714+OkieOth@users.noreply.github.com>
2024-05-19 15:58:32 +0200

    wip: question display

--- tquest/src/ui.rs
+++ tquest/src/ui.rs
@@ -3,14 +3,17 @@ use anyhow::Result;
 use colored::Colorize;
 use std::io;
 
-use crate::questionaire::{QuestionAnswerInput, QuestionEntry};
+use crate::questionaire::{QuestionAnswerInput, QuestionEntry, StringEntry, IntEntry, FloatEntry, BoolEntry, OptionEntry};
 
 
+/// This is returned for normal question entries.
 pub enum QuestionScreenResult {
     Canceled,
     Proceeded(QuestionAnswerInput)
 }
 
+/// This type is returned for questions, where a decision how to
+/// proceed is needed
 pub enum ProceedScreenResult {
     Canceled,
     Proceeded(bool)
@@ -21,6 +24,10 @@ pub trait QuestionaireView {
     fn print_title<'a>(&mut self, title: &str);
     fn show_proceed_screen<'a, T: Into<Option<&'a str>>>(&mut self, id: &str, text: &str, help_text: T) -> Result<ProceedScreenResult>;
     fn show_question_screen(&mut self, question_entry: &QuestionEntry) -> Result<QuestionScreenResult>;
+}
+
+trait ViewHelper {
+    fn get_input_hint(&self) -> String;
 }
 
 
@@ -98,8 +105,98 @@ impl QuestionaireView for Ui {
         }
     }
 
-    fn show_question_screen(&mut self, _question_entry: &QuestionEntry) -> Result<QuestionScreenResult>{
+    fn show_question_screen(&mut self, question_entry: &QuestionEntry) -> Result<QuestionScreenResult>{
+        fn get_valid_input_hint<'b>(question_entry: &QuestionEntry) -> &'b str{
+            if question_entry.help_text.is_some() {
+                "type [y|n] or only ENTER for yes (for more info type 'h')"
+            } else {
+                "type [y|n] or only ENTER for yes"
+            }
+        }
+
+        println!("\n{}", question_entry.query_text.bold());
+        println!("\n{}",get_valid_input_hint(&question_entry).dimmed());
+
         Ok(QuestionScreenResult::Canceled)
     }
 }
 
+
+impl ViewHelper for StringEntry {
+    fn get_input_hint(&self) -> String {
+        let mut s = "Please enter a string and take it with ENTER".to_string();
+        if let Some(def) = self.default_value.as_ref() {
+            s.push_str(&format!(", default: {}", def));
+        }
+        if let Some(min) = self.min_length {
+            s.push_str(&format!(", min-length: {}", min));
+        }
+        if let Some(max) = self.max_length {
+            s.push_str(&format!(", max-length: {}", max));
+        }
+        if let Some(regexp) = self.reqexp.as_ref() {
+            s.push_str(&format!(", regexp: {}", regexp));
+        }
+        s
+    }
+}
+
+impl ViewHelper for IntEntry {
+    // #[derive(Debug, Builder, Clone)]
+    // pub struct IntEntry {
+    //     pub default_value: Option<i32>,
+    //     pub max: Option<i32>,
+    //     pub min: Option<i32>,
+    // }
+    
+    fn get_input_hint(&self) -> String {
+        let mut s = "Please enter an integer and take it with ENTER".to_string();
+        todo!()
+    }
+}
+
+impl ViewHelper for FloatEntry {
+    // #[derive(Debug, Builder, Clone)]
+    // pub struct FloatEntry {
+    //     pub default_value: Option<i32>,
+    //     pub max: Option<f32>,
+    //     pub min: Option<f32>,
+    // }
+    
+    fn get_input_hint(&self) -> String {
+        let mut s = "Please enter a floating point number (e.g. 1.123) and take it with ENTER.".to_string();
+        todo!()
+    }
+}
+
+impl ViewHelper for BoolEntry {
+    fn get_input_hint(&self) -> String {
+        let mut s = "Please enter 'y' for 'yes' or 'n' for 'no'. Take it with ENTER.".to_string();
+        if let Some(def) = self.default_value {
+            let def_str = if def { "[y]es" } else { "[n]o" };
+            s.push_str(&format!(", default: {}", def_str));
+        }
+        s
+    }
+}
+
+impl ViewHelper for OptionEntry {
+    fn get_input_hint(&self) -> String {
+        let mut s = "Please enter the number for one of this options and take it with ENTER.".to_string();
+
+        let default_index = if let Some(def) = self.default_value {
+            def
+        } else {
+            0
+        };
+
+        for (i, o) in self.options.iter().enumerate() {
+            if i == default_index as usize {
+                s.push_str(&format!("\n  [{}] {} (default)", i, o));
+            } else {
+                s.push_str(&format!("\n  [{}] {}", i, o));
+            }
+        }
+        s
+    }
+}
